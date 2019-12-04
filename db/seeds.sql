48a140532d6676846837dfbd54e09f0056d9857e
Hayden Wampach <wampachhayden@gmail.com>
2019-12-03 19:20:48 -0800

    setup files and folders. created sql database and files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Cheese Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Crispy Chicken Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Deluxe Burger', false);
